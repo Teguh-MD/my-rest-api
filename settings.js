7f13b8f8228237121681dba0998d0a913a7ff6ee
Teguh-MD <143882439+Teguh-MD@users.noreply.github.com>
2025-01-28 17:35:29 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
+global.creator = 'teguh-md'// yourname
 global.MONGO_DB_URI = "" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
 global.your_email = "" //email
 global.email_password = "" //application password email
-global.limitCount = 10000
-global.YUOR_PORT = 8000
+global.limitCount = 100
+global.YUOR_PORT = 8080
 global.loghandler = {
 	noapikey:{
 		status: 403,
